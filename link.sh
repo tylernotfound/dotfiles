70ae6a6471445d9353722cde73b8b1b99bc68058
Tyler Smith <hello@tylerbuilds.co>
2020-03-02 14:43:41 -0800

    sort

--- link.sh
+++ link.sh
@@ -4,10 +4,10 @@ ln -sv "~/.dotfiles/.bash_rc" ~
 ln -sv "~/.dotfiles/.editorconfig" ~
 ln -sv "~/.dotfiles/.exports" ~
 ln -sv "~/.dotfiles/.hushlogin" ~
+ln -sv "~/.dotfiles/.inputrc" ~
 ln -sv "~/.dotfiles/.macos" ~
 ln -sv "~/.dotfiles/.screenrc" ~
 ln -sv "~/.dotfiles/.wgetrc" ~
 ln -sv "~/.dotfiles/.zshrc" ~
-ln -sv "~/.dotfiles/.inputrc" ~
 
 ln -sv "~/.dotfiles/.gitconfig" ~
