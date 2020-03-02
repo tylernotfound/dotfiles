3e5f2fec91b75c7bfd03bc57cea39d0dc7f788a7
Tyler Smith <hello@tylerbuilds.co>
2020-03-02 13:54:17 -0800

    changing init file approach

---
+++ link.sh
@@ -0,0 +1,12 @@
+ln -sv "~/.dotfiles/.aliases" ~
+ln -sv "~/.dotfiles/.bash_profile" ~
+ln -sv "~/.dotfiles/.bash_rc" ~
+ln -sv "~/.dotfiles/.editorconfig" ~
+ln -sv "~/.dotfiles/.hushlogin" ~
+ln -sv "~/.dotfiles/.macos" ~
+ln -sv "~/.dotfiles/.screenrc" ~
+ln -sv "~/.dotfiles/.wgetrc" ~
+ln -sv "~/.dotfiles/.zshrc" ~
+ln -sv "~/.dotfiles/.inputrc" ~
+
+ln -sv "~/.dotfiles/.gitconfig" ~
