2c67d8805bb945780ced952d352bd10bb977bcaa
Tyler Smith <hello@tylerbuilds.co>
2020-03-03 09:21:52 -0800

    symlinks don't appear to be working correctly

--- link.sh
+++ link.sh
@@ -1,10 +1,12 @@
-ln -sv "~/.dotfiles/.aliases" ~
-ln -sv "~/.dotfiles/.bash_profile" ~
-ln -sv "~/.dotfiles/.bash_rc" ~
-ln -sv "~/.dotfiles/.editorconfig" ~
-ln -sv "~/.dotfiles/.exports" ~
-ln -sv "~/.dotfiles/.hushlogin" ~
-ln -sv "~/.dotfiles/.inputrc" ~
-ln -sv "~/.dotfiles/.screenrc" ~
-ln -sv "~/.dotfiles/.wgetrc" ~
-ln -sv "~/.dotfiles/.zshrc" ~
+# Not using Symlinks currently, issues with .bash_profile
+# ln -sv "~/.dotfiles/.aliases" ~
+# ln -sv "~/.dotfiles/.bash_profile" ~
+# ln -sv "~/.dotfiles/.bash_rc" ~
+# ln -sv "~/.dotfiles/.editorconfig" ~
+# ln -sv "~/.dotfiles/.exports" ~
+# ln -sv "~/.dotfiles/.gitconfig" ~
+# ln -sv "~/.dotfiles/.hushlogin" ~
+# ln -sv "~/.dotfiles/.inputrc" ~
+# ln -sv "~/.dotfiles/.screenrc" ~
+# ln -sv "~/.dotfiles/.wgetrc" ~
+# ln -sv "~/.dotfiles/.zshrc" ~
